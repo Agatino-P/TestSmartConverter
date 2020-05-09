6a44921b15a8d5f996c5b16d2fcfc143528a54dc
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-09 03:30:18 +0200

    Add project files.

---
+++ TestSmartConverter/SmartCaseConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace TestSmartConverter
+{
+    public enum ConverterCase{None, Upper, Lower }
+    public class SmartCaseConverter : IValueConverter
+    {
+        public ConverterCase ConverterCase { get; set; } = ConverterCase.None;
+
+        public object  Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (ConverterCase)
+            {
+                case ConverterCase.None:
+                    return $"None { value.ToString()}";
+                case ConverterCase.Upper:
+                    return $"Upper {value.ToString().ToUpperInvariant()}";
+                case ConverterCase.Lower:
+                    return $"Lower {value.ToString().ToLowerInvariant()}";
+                default:
+                    return "string.Empty";
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
